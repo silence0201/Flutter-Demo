5cf5791c674a599f736d55059cbc307ec175b971
Silence <374619540@qq.com>
2019-11-23 20:26:51 +0800

    Dio

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,12 +7,73 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final typeController = TextEditingController();
+  String showText = "欢迎,Hello,World";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('首页'),
+      appBar: AppBar(
+        title: Text('Hello,Wrold'),
       ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: typeController,
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10.0),
+                    labelText: '类型',
+                    helperText: '请输入类型'
+                ),
+                autofocus: false,
+              ),
+              RaisedButton(
+                onPressed: _choicAction,
+                child: Text('选择完毕'),
+              ),
+              Text(
+                showText,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              )
+            ],
+          ),
+        ),
+      )
+
     );
+  }
+
+  void _choicAction() {
+    print("开始选择");
+    if (typeController.text.toString() == '') {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(title: Text('类型不能为空'),)
+      );
+    } else {
+      getHttp(typeController.text.toString()).then((vale) {
+        setState(() {
+          showText = vale.toString();
+        });
+      });
+    }
+  }
+
+  Future getHttp(String typeText) async {
+    try {
+      Response response;
+      var data = {'name' : typeText};
+      response = await Dio().get(
+        "https://www.baidu.com",
+        queryParameters: data
+      );
+      return response.data;
+    } catch (e) {
+      print(e);
+    }
   }
 }
